3bbba2b35393a128aa6b75d2158999a9d10fb828
simranjitsit24-code <simranjits.it.24@nitj.ac.in>
2026-07-09 12:55:30 +0530

    Update server.js

--- Profile-Card-main/server/server.js
+++ Profile-Card-main/server/server.js
@@ -30,12 +30,7 @@ app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
-// If you want to serve the React frontend from the same server:
-// (only if you have built your React app and copied the 'dist' folder into the server folder)
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
